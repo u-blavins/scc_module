2e366027b8fa01164d3f6a3b6e6e305488d68088
u-blavins <usama.blavins1@gmail.com>
2019-12-29 16:11:01 +0800

    Update SharesBrokerWS.java with listShares functionality

--- coursework/SharesBrokerWSApplication/src/java/sharesbroker/SharesBrokerWS.java
+++ coursework/SharesBrokerWSApplication/src/java/sharesbroker/SharesBrokerWS.java
@@ -5,9 +5,13 @@
  */
 package sharesbroker;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.xml.bind.JAXBException;
 
 /**
  *
@@ -18,9 +22,35 @@ public class SharesBrokerWS {
 
     /**
      * This is a sample web service operation
+     * @return currentCompanyShares
      */
-    @WebMethod(operationName = "TestService")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    @WebMethod(operationName="listShares")
+    public CompanyShares listShares() {
+        CompanyShares currentCompanyShares = new CompanyShares();
+        
+        try {
+            javax.xml.bind.JAXBContext jaxbContext = 
+                    javax.xml.bind.JAXBContext.newInstance(
+                            currentCompanyShares.getClass().getPackage().getName());
+            javax.xml.bind.Unmarshaller unmarshaller = 
+                    jaxbContext.createUnmarshaller();
+            File file = new File("/Users/UBlavins/ntu_year3/scc_module/coursework/SharesBrokerWSApplication/Files/shares.xml");
+            currentCompanyShares = (CompanyShares) unmarshaller.unmarshal(file);
+        } catch (javax.xml.bind.JAXBException ex) {
+            java.util.logging.Logger.getLogger("global").log(java.util.logging.Level.SEVERE, null, ex); //NOI18N
+        }
+        return currentCompanyShares;
+    }
+    
+    @WebMethod(operationName = "listAvailableShares")
+    public int listAvailableShares(@WebParam(name = "companyName")String companyName) throws JAXBException {
+        int availableShares = 0;
+        List<ShareType> companyShares = listShares().getShares();
+        for (ShareType share : companyShares) {
+            if (share.getCompanyName().equals(companyName)){
+                availableShares = share.getAvailableShares();
+            }
+        }
+        return availableShares;
     }
 }
